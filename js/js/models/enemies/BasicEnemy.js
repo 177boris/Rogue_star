f120ac8d443fa4c99d8e487d2e118498dea132f8
177boris <85952901+177boris@users.noreply.github.com>
2021-07-20 03:53:25 +0100

    Delete js/js directory

--- js/js/models/enemies/BasicEnemy.js
+++
@@ -1,3 +0,0 @@
-class BasicEnemy extends Enemy {
-
-}
--- js/js/models/enemies/Enemy.js
+++
@@ -1,9 +0,0 @@
-class Enemy {
-    constructor(currentScene, startx, starty) {
-        this.scene = currentScene;
-        this.health = 100;
-        this.sprite = currentScene.physics.add.sprite(startx, starty, "enemyAtlas", "enemy-front")
-            .setSize(30, 40)
-            .setOffset(0, 24);
-    }
-}
